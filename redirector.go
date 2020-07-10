ea7867818d1e75d843e52ff428c5170af5061145
chrissx <chrissx@chrissx.de>
2020-07-10 23:07:33 +0200

    fixed stupidity

--- redirector.go
+++ redirector.go
@@ -8,10 +8,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Got an %s request from %s: %s",
-			r.Proto, r.RemoteAddr, r.URL)
-		url := r.URL
-		url.Scheme = "https"
+		log.Printf("Got an %s request from %s: %s (%s)",
+			r.Proto, r.RemoteAddr, r.URL, r.Host)
 		fmt.Fprintf(w, "<html>")
 		fmt.Fprintf(w, "<head>")
 		fmt.Fprintf(w, "<title>Redirecting...</title>")
@@ -21,8 +19,6 @@ func main() {
 		fmt.Fprintf(w, "</head>")
 		fmt.Fprintf(w, "<body>")
 		fmt.Fprintf(w, "Just switch to https up there ↑")
-		fmt.Fprintf(w, "<br/>")
-		fmt.Fprintf(w, "Or click <a href='%s'>here</a>", url)
 		fmt.Fprintf(w, "</body>")
 		fmt.Fprintf(w, "</html>")
 	})

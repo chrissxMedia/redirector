5478ad83299fcf0c27c92ec073c64c22348be672
chrissx <chrissx@chrissx.de>
2020-07-10 20:49:29 +0200

    basic implementation, because yes

---
+++ redirector.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Got a request over %s from %s: %s.",
+			r.Proto, r.RemoteAddr, r.URL)
+		fmt.Fprintf(w, "<html><head><title>Redirecting...</title><script>window.location.protocol = 'https:'</script></head><body>Just replace that \"http://\" with a \"https://\" up there ^</body></html>")
+	})
+
+	http.ListenAndServe(":8086", nil)
+}

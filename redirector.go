2f37e39a52d77fc93d8f6fb174e8735a1042d0be
pixel <chrissx@chrissx.de>
2022-04-23 17:42:42 +0200

    prometheus support

--- redirector.go
+++ redirector.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -20,9 +23,25 @@ Just switch to https up there ↑
 </body>
 </html>
 `
+
+	var totalReqs = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "redirector_total_requests",
+		Help: "Total number of HTTP requests coming in.",
+	}, []string{})
+
+	var hostReqs = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "redirector_host_requests",
+		Help: "Number of HTTP requests that contain the Host header.",
+	}, []string{})
+
+	prometheus.MustRegister(totalReqs)
+	prometheus.MustRegister(hostReqs)
+	http.Handle("/metrics", promhttp.Handler())
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Got a %s request from %s: %s (%s)",
 			r.Proto, r.RemoteAddr, r.URL, r.Host)
+		totalReqs.WithLabelValues().Inc()
 		// this matches urls like chrissx.de.evil.com, but
 		// there are no ways to exploit that (except if there
 		// are other misdesigns)
@@ -36,6 +55,7 @@ Just switch to https up there ↑
 			url.Path = r.URL.Path
 			w.Header().Add("Location", url.String())
 			w.WriteHeader(307)
+			hostReqs.WithLabelValues().Inc()
 		}
 		fmt.Fprintf(w, response)
 	})

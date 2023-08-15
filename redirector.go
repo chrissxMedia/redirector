4239466077f1ea3fc7f006f68ac07fae2e3b35d2
pixel <pixel+github@chrissx.de>
2023-08-15 22:59:38 +0200

    add `<meta>` `refresh`
    an additional fallback, as the mission for this project remains
    to implement pretty much every standard way of https upgrading

--- redirector.go
+++ redirector.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"html"
 	"net/http"
 	"net/url"
 	"strings"
@@ -29,13 +30,15 @@ func main() {
 
 	cm3.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		totalReqs.WithLabelValues().Inc()
+		refresh := ""
 		if strings.Contains(r.Host, ".") && !strings.Contains(r.Host, ":") {
 			var url = url.URL{Host: r.Host, Scheme: "https", Path: r.URL.Path}
 			w.Header().Add("Location", url.String())
 			w.WriteHeader(301)
 			hostReqs.WithLabelValues().Inc()
+			refresh = "<meta http-equiv=\"refresh\" content=\"0;url=" + html.EscapeString(url.String()) + "\"/>"
 		}
-		fmt.Fprintf(w, response)
+		fmt.Fprintf(w, response, refresh)
 	})
 
 	cm3.ListenAndServeHttp(":80", nil)
